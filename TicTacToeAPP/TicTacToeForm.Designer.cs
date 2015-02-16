2b6a74ce16561b162b3702bd985f636076f989d8
Magnatj <felipeglez93@hotmail.com>
2015-02-15 19:23:17 -0800

    Initial Commit TicTacToe

---
+++ TicTacToeAPP/TicTacToeForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace TicTacToeAPP
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTicTac = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelOptions = new System.Windows.Forms.Panel();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.panelTicTac.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelTicTac
+            // 
+            this.panelTicTac.BackColor = System.Drawing.Color.White;
+            this.panelTicTac.BackgroundImage = global::TicTacToeAPP.Properties.Resources.TicTacToeBoard;
+            this.panelTicTac.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelTicTac.Controls.Add(this.pb9);
+            this.panelTicTac.Controls.Add(this.pb8);
+            this.panelTicTac.Controls.Add(this.pb7);
+            this.panelTicTac.Controls.Add(this.pb6);
+            this.panelTicTac.Controls.Add(this.pb4);
+            this.panelTicTac.Controls.Add(this.pb3);
+            this.panelTicTac.Controls.Add(this.pb2);
+            this.panelTicTac.Controls.Add(this.pb1);
+            this.panelTicTac.Controls.Add(this.pb5);
+            this.panelTicTac.Controls.Add(this.panel3);
+            this.panelTicTac.Location = new System.Drawing.Point(0, 0);
+            this.panelTicTac.Name = "panelTicTac";
+            this.panelTicTac.Size = new System.Drawing.Size(482, 452);
+            this.panelTicTac.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(2, 451);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 10);
+            this.panel3.TabIndex = 0;
+            // 
+            // panelOptions
+            // 
+            this.panelOptions.BackColor = System.Drawing.Color.Aquamarine;
+            this.panelOptions.Location = new System.Drawing.Point(0, 458);
+            this.panelOptions.Name = "panelOptions";
+            this.panelOptions.Size = new System.Drawing.Size(482, 120);
+            this.panelOptions.TabIndex = 1;
+            // 
+            // pb5
+            // 
+            this.pb5.Location = new System.Drawing.Point(171, 163);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(131, 131);
+            this.pb5.TabIndex = 1;
+            this.pb5.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(3, 0);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(131, 131);
+            this.pb1.TabIndex = 2;
+            this.pb1.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(171, 0);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(131, 131);
+            this.pb2.TabIndex = 3;
+            this.pb2.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.Location = new System.Drawing.Point(343, 0);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(131, 131);
+            this.pb3.TabIndex = 4;
+            this.pb3.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(3, 163);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(131, 131);
+            this.pb4.TabIndex = 5;
+            this.pb4.TabStop = false;
+            // 
+            // pb6
+            // 
+            this.pb6.Location = new System.Drawing.Point(343, 163);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(131, 131);
+            this.pb6.TabIndex = 6;
+            this.pb6.TabStop = false;
+            // 
+            // pb7
+            // 
+            this.pb7.Location = new System.Drawing.Point(3, 321);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(131, 131);
+            this.pb7.TabIndex = 7;
+            this.pb7.TabStop = false;
+            // 
+            // pb8
+            // 
+            this.pb8.Location = new System.Drawing.Point(171, 321);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(131, 131);
+            this.pb8.TabIndex = 8;
+            this.pb8.TabStop = false;
+            // 
+            // pb9
+            // 
+            this.pb9.Location = new System.Drawing.Point(343, 321);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(131, 131);
+            this.pb9.TabIndex = 9;
+            this.pb9.TabStop = false;
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(482, 578);
+            this.Controls.Add(this.panelOptions);
+            this.Controls.Add(this.panelTicTac);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "TicTacToeForm";
+            this.Text = "Tic Tac Toe";
+            this.panelTicTac.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTicTac;
+        private System.Windows.Forms.Panel panelOptions;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb5;
+    }
+}
